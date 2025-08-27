bf62bb46ca460f25f833a00bc2b3f7f1dad591c3
CMDESIGN8 <cm@lowcargo.lat>
2025-08-27 17:52:00 +0000

    10111

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabase.js";
+import { supabase } from "../lib/supabaseClient.js";
 import { createPlayer, getPlayer } from "../services/players";
 
 export const useAuth = () => {
