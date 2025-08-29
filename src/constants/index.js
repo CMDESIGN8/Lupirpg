65b45fb27385b61dd4fdcf6a1eb40c9197a83fe9
CMDESIGN8 <cm@lowcargo.lat>
2025-08-29 15:02:47 +0000

    ad #	modified:   src/constants/index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
+export const positions = ['Arquero', 'Defensa', 'Mediocampista', 'Delantero', 'Neutro'];
 export const positionsBySport = {
   Fútbol: [
     'Arquero',
@@ -56,7 +57,6 @@ export const positionsBySport = {
     'Entrenamiento Funcional'
   ]
 };
-
 export const sports = ['Fútbol', 'Voley', 'Handball', 'Hockey', 'Rugby', 'Fitness'];
 export const skillNames = ["Fuerza", "Resistencia", "Técnica", "Velocidad", "Dribling", "Pase", "Tiro", "Defensa", "Liderazgo", "Estrategia", "Inteligencia"];
 export const initialSkillPoints = 10;
