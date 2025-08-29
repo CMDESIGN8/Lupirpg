0aa2c7bc04e2d36d20372d852aa8891b7c92a887
CMDESIGN8 <cm@lowcargo.lat>
2025-08-29 14:25:04 +0000

    as

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,65 @@
-export const positions = ['Arquero', 'Defensa', 'Mediocampista', 'Delantero', 'Neutro'];
 export const sports = ['Fútbol', 'Voley', 'Handball', 'Hockey', 'Rugby', 'Fitness'];
+
+export const positions = {
+  Fútbol: [
+    'Arquero',
+    'Defensa Central',
+    'Lateral',
+    'Mediocampista Defensivo',
+    'Mediocampista Ofensivo',
+    'Extremo',
+    'Delantero Centro',
+    'Segundo Delantero'
+  ],
+  Voley: [
+    'Armador',
+    'Opuesto',
+    'Central',
+    'Punta Receptor',
+    'Libero'
+  ],
+  Handball: [
+    'Arquero',
+    'Extremo',
+    'Lateral',
+    'Central',
+    'Pivote'
+  ],
+  Hockey: [
+    'Arquero',
+    'Defensa Central',
+    'Defensa Lateral',
+    'Volante Central',
+    'Volante Ofensivo',
+    'Delantero',
+    'Centrodelantero'
+  ],
+  Rugby: [
+    'Pilar',
+    'Hooker',
+    'Segunda Línea',
+    'Ala',
+    'Octavo',
+    'Medio Scrum',
+    'Apertura',
+    'Centro',
+    'Wing',
+    'Fullback'
+  ],
+  Fitness: [
+    'Powerlifter',
+    'Crossfitter',
+    'Calistenia',
+    'Corredor',
+    'Ciclista',
+    'Nadador',
+    'Boxeador',
+    'Artes Marciales',
+    'Entrenamiento Funcional'
+  ]
+};
+
 export const skillNames = ["Fuerza", "Resistencia", "Técnica", "Velocidad", "Dribling", "Pase", "Tiro", "Defensa", "Liderazgo", "Estrategia", "Inteligencia"];
-export const initialSkillPoints = 5;
+export const initialSkillPoints = 10;
 export const SUPABASE_URL = "https://xvdevkrgsgiiqqhfnnut.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2ZGV2a3Jnc2dpaXFxaGZubnV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3MzMwMDQsImV4cCI6MjA3MTMwOTAwNH0.uS3WC9rdNeAeGmiJdwKC-q1N_w_rDE413Zu62rfmLVc";
