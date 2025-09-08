90ad8b9bf3af5adfc198a41503d50a12b1952010
CMDESIGN8 <cm@lowcargo.lat>
2025-09-08 19:07:25 +0000

    a ver como se ve el mercado de ao

--- src/components/Views/DashboardView.jsx
+++ src/components/Views/DashboardView.jsx
@@ -411,7 +411,7 @@ const DashboardView = ({
         <h2 className="Merket-title">Tienda y Wallet</h2>
         <div className="Market-line"></div>
         <MarketView 
-          MarketItems={fetchMarketItems} 
+          marketItems={fetchMarketItems} 
           playerData={playerData} 
           loading={loading} 
           message={message} 
