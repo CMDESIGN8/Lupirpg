9f4d6c8840af8b1cbb7b7e4d3e046af2f15f103d
CMDESIGN8 <cm@lowcargo.lat>
2025-10-01 20:05:30 +0000

    qwe w

--- src/components/Views/CommonRoomModal.jsx
+++ src/components/Views/CommonRoomModal.jsx
@@ -1,6 +1,6 @@
 // CommonRoomModal.jsx
 import React, { useState } from "react";
-import CommonRoom3D from "./CommonRoom"; // Tu componente 3D
+import CommonRoom3D from "../game/CommonRoom.jsx"; // Tu componente 3D
 import "./CommonRoom.css"; // Tus estilos del dashboard
 
 export default function CommonRoomModal({ isOpen, onClose }) {
